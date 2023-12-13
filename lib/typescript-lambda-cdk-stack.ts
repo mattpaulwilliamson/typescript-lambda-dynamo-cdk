b7ef09ff95f495cfd6a09df17c02c3cb39ed0e86
Matthew Williamson <Matt.williamson@deadhappy.com>
2023-12-13 15:19:40 +0000

    Initial commit

---
+++ lib/typescript-lambda-cdk-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class TypescriptLambdaCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'TypescriptLambdaCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
